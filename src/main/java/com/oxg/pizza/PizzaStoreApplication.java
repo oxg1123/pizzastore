688849e01d8dd23f707aad51ba6fc699f7f17f60
olegg <oxg1123@yahoo.com>
2018-02-21 11:17:45 -0800

    initial commit

---
+++ src/main/java/com/oxg/pizza/PizzaStoreApplication.java
@@ -0,0 +1,12 @@
+package com.oxg.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaStoreApplication.class, args);
+	}
+}
